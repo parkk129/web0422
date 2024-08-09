6e3dc6484c29f2fc272a9b93fc4ec867cb985797
parkk129 <seohyun3517@naver.com>
2024-08-09 09:58:08 +0900

    0809

---
+++ 50_js/auto_commit.bat
@@ -0,0 +1,18 @@
+:loop
+
+   cd C:\Users\USER\Documents\GitHub\ju-ing
+
+   git init
+   
+   git pull
+   
+   git add --all
+   
+   git commit -m "auto commit"
+   
+   git push
+   
+   :: 3600초 대기
+   TIMEOUT 3600
+   
+goto loop
