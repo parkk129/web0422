aeee08084ed20cfd6a41b9196176057d59145d32
parkk129 <167740291+parkk129@users.noreply.github.com>
2025-05-28 23:40:55 +0900

    0528

--- 웹디자인기능사/js/script.js
+++ 웹디자인기능사/js/script.js
@@ -1,6 +1,46 @@
 $(()=>{
 
+    // nav
+
+    $(".nav > ul > li").mouseover(function(){
+
+        $(".submenu").stop().fadeIn();
+        $("#slider").addClass("on");
+
+    });
+
+    $(".nav > ul > li").mouseout(function(){
+
+        $(".submenu").stop().fadeOut();
+        $("#slider").removeClass("on");
+
+    });
+
+
+
+
+// slider
+
+let currentIndex = 0;
+
+setInterval(()=>{
+
+    currentIndex++;
+
+    $(".sliderWrap").animate({marginTop : - 350 * currentIndex + "px"}, 600);
     
+    if(currentIndex == 3){
+
+        setTimeout(()=>{
+            $(".sliderWrap").animate({marginTop:0 }, 0);
+        },700)
+
+        currentIndex = 0;
+    }
+
+
+}, 3000)
+
 
 
 });
